745e66bf4de0ec2b6d7c7e781aa13289ae5ec080
joshdiduca <josh@di-duca.co.uk>
2025-04-26 14:15:04 +0100

    v2.0.7

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,38 @@
-import { app } from 'electron'
+import { app, ipcMain } from 'electron'
 import log from 'electron-log'
 log.initialize()
 console.log = log.log // Replace default console
 
 import { makeAppSetup, makeAppWithSingleInstanceLock } from './factories'
 import { MainWindow, registerAboutWindowCreationByIPC } from './windows'
+import { ChromaDBService } from './services/chroma/chromaDBService'
 
 makeAppWithSingleInstanceLock(async () => {
   await app.whenReady()
+
+  const chromaService = new ChromaDBService()
+  const ollamaService = new OllamaService()
+
+  // Create your window here
+
+  // Setup IPC handlers
+  ipcMain.handle('start-chromadb', async () => {
+    await chromaService.start()
+    return true
+  })
+
+  ipcMain.handle('get-chromadb-config', () => {
+    return chromaService.getClientConfig()
+  })
+
+  // Clean up on quit
+  app.on('will-quit', async (e) => {
+    e.preventDefault()
+    await chromaService.stop()
+    await ollamaService.stop()
+    app.exit()
+  })
+
   await makeAppSetup(MainWindow)
 
   registerAboutWindowCreationByIPC()

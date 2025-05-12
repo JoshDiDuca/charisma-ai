6656e33da248e0b90d0dccfdc03e9999b3d5475e
joshdiduca <josh@di-duca.co.uk>
2025-05-12 23:55:43 +0100

    Adding getting manifest urls - use for later

--- src/main/services/ollama/ollamaCatalog.ts
+++ src/main/services/ollama/ollamaCatalog.ts
@@ -1,4 +1,4 @@
-import { OllamaModel } from "shared/types/OllamaModel";
+import { OllamaExtraction, OllamaModel } from "shared/types/OllamaModel";
 
 export const OllamaModels: OllamaModel[] = [
   {
@@ -79,3 +79,60 @@ export const OllamaModels: OllamaModel[] = [
   }
 
 ]
+
+export const getUrls = async (baseUrl: string, manifestUrl: string) => {
+      try {
+        const response = await fetch(manifestUrl, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch manifest data');
+        }
+
+        const manifestData = await response.json();
+
+        const urls = {
+          manifest: manifestUrl,
+          config: baseUrl + manifestData.config.digest,
+          layers: manifestData.layers.map((layer: any) => ({
+            url: baseUrl + layer.digest,
+            size: layer.size,
+            mediaType: layer.mediaType,
+            filename: layer.digest.replace(":", "-")
+          })),
+          configFilename: manifestData.config.digest.replace(":", "-")
+        };
+
+        return urls;
+      } catch (error) {
+        throw new Error(`Error fetching model data: ${error}`);
+      }
+    }
+
+
+export const extractOllamaDownloadUrls = async (modelInput: string) : Promise<OllamaExtraction> => {
+  // Convert input to model name and tag
+  const inputSplit = modelInput.trim().split(':');
+  if (inputSplit.length !== 2) {
+    throw new Error('Invalid input format. Please use the format "model:tag"');
+  }
+
+  const modelName = inputSplit[0];
+  const tag = inputSplit[1];
+  const manifestUrl = `https://registry.ollama.ai/v2/library/${modelName}/manifests/${tag}`;
+  const baseUrl = `https://registry.ollama.ai/v2/library/${modelName}/blobs/`;
+
+ const urls = await getUrls(baseUrl, manifestUrl);
+
+  return {
+    modelName,
+    tag,
+    manifestUrl,
+    baseUrl,
+    ...urls
+  };
+}
--- src/shared/types/OllamaModel.ts
+++ src/shared/types/OllamaModel.ts
@@ -1,3 +1,19 @@
 export type OllamaModelType = "LLM" | "Embedding" | "Reasoning";
 
 export type OllamaModel = { name: string, type: OllamaModelType, installed?: boolean, installing?: boolean }
+
+export type OllamaExtraction = {
+    manifestUrl: string;
+    baseUrl: string;
+    config: string;
+    modelName: string;
+    tag: string;
+    layers: {
+      url: string;
+            size: number;
+            mediaType: string;
+            filename: string;
+
+    }[];
+    configFilename: any;
+}

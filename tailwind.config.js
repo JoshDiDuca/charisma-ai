32216f6a9db9fcae13800f2d9dc67d80e7699053
joshdiduca <josh@di-duca.co.uk>
2025-05-23 00:57:26 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     // ...
     // make sure it's pointing to the ROOT node_module
+    "./src/renderer/**/*.{js,ts,jsx,tsx}",
     './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {

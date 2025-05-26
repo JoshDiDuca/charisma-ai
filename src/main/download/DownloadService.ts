90850cb83938ee18952da9cf3cb76adb9a902677
joshdiduca <josh@di-duca.co.uk>
2025-05-26 16:11:33 +0100

    Delete zip file from download service after zip

--- src/main/download/DownloadService.ts
+++ src/main/download/DownloadService.ts
@@ -171,7 +171,7 @@ export class DownloadService extends EventEmitter {
 
       return finalFile
     } finally {
-      await this.cleanupTemp(tempFile)
+      await this.cleanupTemp(tempFile, finalFile)
     }
 
   }
@@ -263,6 +263,9 @@ export class DownloadService extends EventEmitter {
 
   private async cleanupTemp(tempPath: string, finalFile?: string): Promise<void> {
     await deleteFileOrFolder(tempPath);
+    if(finalFile){
+      await deleteFileOrFolder(finalFile);
+    }
   }
   private async moveContents(source: string, target: string) {
     // Add existence check

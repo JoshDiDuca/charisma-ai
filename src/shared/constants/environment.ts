4d59dcc36259c9ccdb7de1fe905dce6f8520358e
joshdiduca <josh@di-duca.co.uk>
2025-08-15 04:22:35 +0100

    More default ignore paths

--- src/shared/constants/environment.ts
+++ src/shared/constants/environment.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
   IS_DEV: process.env.NODE_ENV === 'development',
   DISABLE_TTS_ON_START: true,
-  DEFAULT_IGNORE_FOLDERS: ["node_modules", "dist", "build", "bin", ".git", ".vscode", "venv", "env", "lib", "lib64", "__pycache__", ".idea"],
+  DEFAULT_IGNORE_FOLDERS: ["node_modules", "dist", "build", "bin", ".git", ".vscode", "venv", "env", "lib", "lib64", "__pycache__", ".idea", "yarn.lock", "package-lock.json", "pnpm-lock.yaml", ".DS_Store", ".next", ".nuxt", ".cache", ".vercel", ".output", "coverage"],
 }
